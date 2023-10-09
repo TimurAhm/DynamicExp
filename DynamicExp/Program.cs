d6b3f0045d8ed22bd9e6075cdc10e198a9debe2e
Timur <User@LAPTOP-Q3NBDU5U>
2023-10-09 17:37:17 +0400

    Add project files.

---
+++ DynamicExp/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        dynamic value;
+        for (int demo = 0; demo < 3; demo++)
+        {
+            value = (demo < 2) ? (dynamic) 5 : (dynamic) "A";
+            value = value + value;
+            Console.WriteLine(value);
+        }
+    }
+
+    private static void M(Int32 n) { Console.WriteLine("M(Int32) : " + n); }
+    private static void M(String s) { Console.WriteLine("M(String) : " + s); }
+}
